0b7afb21d6aaa93d9019c33735340148debe785f
balaji <balaji.chopparapu@gmail.com>
2020-01-27 20:20:05 +0530

    fixed tow q values

--- incrementality.py
+++ incrementality.py
@@ -15,20 +15,27 @@ import pandas as pd
 
 
 def get_q_value(dataset):
-    # print(dataset)
-    theta_mean = dataset['log_rr'].mean()
-    dataset['log_rr_mean'] = theta_mean
-    dataset['q_i'] = dataset['w'] * ((dataset['log_rr'] - theta_mean) ** 2)
-    dataset['q'] = dataset['q_i'].sum()
-    return dataset['q_i'].sum()
+    return dataset['w_log_rr_sq'].sum() - ((dataset['w_log_rr'].sum() ** 2) / dataset['w'].sum())
 
 
 def get_tow_value(dataset, q):
     tow = 0
     k = len(dataset)
     if q > k - 1:
-        tow = dataset['w'].sum() - (dataset['w_sq'].sum() / dataset['w'].sum())
+        tow = (q-k-1)/(dataset['w'].sum() - (dataset['w_sq'].sum() / dataset['w'].sum()))
     return tow
+
+
+def apply_zero_correction_factor(row):
+    if (row["ctrl_conv"] == 0):
+        # row["zc_ctrl_conv"] = 1 / row["control_reach"]
+        row["zc_ctrl_conv"] = 0.5
+        row["zc_control_reach"] = row["control_reach"] + 1
+    if (row["test_conv"] == 0):
+        # row["zc_test_conv"] = 1 / row["test_reach"]
+        row["zc_test_conv"] = 0.5
+        row["zc_test_reach"] = row["test_reach"] + 1
+    return row
 
 
 if __name__ == '__main__':
@@ -43,10 +50,12 @@ if __name__ == '__main__':
     zc = 0.5
 
     # Adding zero correction factor
-    beer_hawk_inc["zc_ctrl_conv"] = beer_hawk_inc["ctrl_conv"] + 0.5
-    beer_hawk_inc["zc_control_reach"] = beer_hawk_inc["control_reach"] + 1
-    beer_hawk_inc["zc_test_reach"] = beer_hawk_inc["test_reach"] + 1
-    beer_hawk_inc["zc_test_conv"] = beer_hawk_inc["test_conv"] + 0.5
+
+    beer_hawk_inc["zc_ctrl_conv"] = beer_hawk_inc["ctrl_conv"]
+    beer_hawk_inc["zc_control_reach"] = beer_hawk_inc["control_reach"]
+    beer_hawk_inc["zc_test_reach"] = beer_hawk_inc["test_reach"]
+    beer_hawk_inc["zc_test_conv"] = beer_hawk_inc["test_conv"]
+    beer_hawk_inc = beer_hawk_inc.apply(apply_zero_correction_factor, axis=1)
 
     # sample proportions
     beer_hawk_inc["pc"] = beer_hawk_inc["zc_ctrl_conv"] / beer_hawk_inc["zc_control_reach"]
@@ -73,6 +82,10 @@ if __name__ == '__main__':
     beer_hawk_inc['w'] = 1 / beer_hawk_inc['vari']
     beer_hawk_inc['w_sq'] = beer_hawk_inc['w'] ** 2
 
+    beer_hawk_inc['w_log_rr'] = beer_hawk_inc['w'] * beer_hawk_inc['log_rr']
+    beer_hawk_inc['log_rr_sq'] = beer_hawk_inc['log_rr'] ** 2
+    beer_hawk_inc['w_log_rr_sq'] = beer_hawk_inc['w'] * beer_hawk_inc['log_rr_sq']
+
     # Meta-analysis of Adset
     # Group them by dimension and calculate Aggregate measure
     for dimension, df_dim_dateset in beer_hawk_inc.groupby('dimension_type'):
@@ -92,9 +105,6 @@ if __name__ == '__main__':
         agg_ci_ub = 1 - exp(agg_theta - (1.96 * sqrt(agg_variance)))
         print('For dimension:', dimension)
         print('Aggregate incrementality: ', agg_inc)
-        print('confidence interval lower bound: ', agg_ci_lb)
-        print('confidence interval upper bound: ', agg_ci_ub)
-
-print('Done')
-
- 
+        #print('confidence interval lower bound: ', agg_ci_lb)
+        #print('confidence interval upper bound: ', agg_ci_ub)
+    print('Done')
