75cbf82ad30920760edf3654d057e631ac235907
balaji <balaji.chopparapu@gmail.com>
2020-01-29 23:04:20 +0530

    fixed tow q values

--- incrementality.py
+++ incrementality.py
@@ -14,15 +14,15 @@ import numpy as np
 import pandas as pd
 
 
-def get_q_value(dataset):
-    return dataset['w_log_rr_sq'].sum() - ((dataset['w_log_rr'].sum() ** 2) / dataset['w'].sum())
+def get_q_value(data):
+    return data['w_log_rr_sq'].sum() - ((data['w_log_rr'].sum() ** 2) / data['w'].sum())
 
 
-def get_tow_value(dataset, q):
+def get_tow_value(data, q):
     toue = 0
-    k = len(dataset)
+    k = len(data)
     if q > k - 1:
-        toue = (q - k - 1) / (dataset['w'].sum() - (dataset['w_sq'].sum() / dataset['w'].sum()))
+        toue = (q - k - 1) / (data['w'].sum() - (data['w_sq'].sum() / data['w'].sum()))
     return toue
 
 
@@ -60,57 +60,57 @@ def apply_zero_correction_factor_static(row):
 if __name__ == '__main__':
     print('Begin Analysis')
     os.chdir("/home/mario/work/measured")
-    beer_hawk = pd.read_excel("/home/mario/work/measured/Beer Hawk.xlsx", sheet_name=0)
+    dataset_orginal = pd.read_excel("/home/mario/work/measured/Beer Hawk.xlsx", sheet_name=0)
     reqvars = ['dimension_type', 'dimension_value', 'cuts', 'interval_start_date', 'interval_end_date',
                'control_reach', 'ctrl_conv', 'test_reach', 'test_conv']
-    beer_hawk_inc = beer_hawk.loc[:, reqvars]
+    dataset = dataset_orginal.loc[:, reqvars]
 
     # Zero correction factor
     zc = 0.5
 
     # Adding zero correction factor
 
-    beer_hawk_inc["zc_ctrl_conv"] = beer_hawk_inc["ctrl_conv"]
-    beer_hawk_inc["zc_control_reach"] = beer_hawk_inc["control_reach"]
-    beer_hawk_inc["zc_test_reach"] = beer_hawk_inc["test_reach"]
-    beer_hawk_inc["zc_test_conv"] = beer_hawk_inc["test_conv"]
-    beer_hawk_inc = beer_hawk_inc.apply(apply_zero_correction_factor, axis=1)
+    dataset["zc_ctrl_conv"] = dataset["ctrl_conv"]
+    dataset["zc_control_reach"] = dataset["control_reach"]
+    dataset["zc_test_reach"] = dataset["test_reach"]
+    dataset["zc_test_conv"] = dataset["test_conv"]
+    dataset = dataset.apply(apply_zero_correction_factor, axis=1)
 
     # sample proportions
-    beer_hawk_inc["pc"] = beer_hawk_inc["zc_ctrl_conv"] / beer_hawk_inc["zc_control_reach"]
-    beer_hawk_inc["pt"] = beer_hawk_inc["zc_test_conv"] / beer_hawk_inc["zc_test_reach"]
+    dataset["pc"] = dataset["zc_ctrl_conv"] / dataset["zc_control_reach"]
+    dataset["pt"] = dataset["zc_test_conv"] / dataset["zc_test_reach"]
 
     # Risk ratio
-    beer_hawk_inc["rr"] = beer_hawk_inc["pc"] / beer_hawk_inc["pt"]
+    dataset["rr"] = dataset["pc"] / dataset["pt"]
 
     # incrementality
-    beer_hawk_inc["inc"] = 1 - beer_hawk_inc["rr"]
+    dataset["inc"] = 1 - dataset["rr"]
 
     # logarithm Risk ratio
-    beer_hawk_inc["log_rr"] = np.log(beer_hawk_inc["rr"])
+    dataset["log_rr"] = np.log(dataset["rr"])
 
     # variance
-    beer_hawk_inc['vari'] = (1 / beer_hawk_inc["zc_ctrl_conv"]) - (1 / beer_hawk_inc["zc_control_reach"]) + (
-            1 / beer_hawk_inc["zc_test_conv"]) - (1 / beer_hawk_inc["zc_test_reach"])
+    dataset['vari'] = (1 / dataset["zc_ctrl_conv"]) - (1 / dataset["zc_control_reach"]) + (
+            1 / dataset["zc_test_conv"]) - (1 / dataset["zc_test_reach"])
 
     # Confidence interval
-    beer_hawk_inc["ci_u"] = 1 - np.exp(beer_hawk_inc["log_rr"] - (1.96 * np.sqrt(beer_hawk_inc["vari"])))
-    beer_hawk_inc["ci_l"] = 1 - np.exp(beer_hawk_inc["log_rr"] + (1.96 * np.sqrt(beer_hawk_inc["vari"])))
+    dataset["ci_u"] = 1 - np.exp(dataset["log_rr"] - (1.96 * np.sqrt(dataset["vari"])))
+    dataset["ci_l"] = 1 - np.exp(dataset["log_rr"] + (1.96 * np.sqrt(dataset["vari"])))
 
     # Weight of a study of each study
-    beer_hawk_inc['w'] = 1 / beer_hawk_inc['vari']
-    beer_hawk_inc['w_sq'] = beer_hawk_inc['w'] ** 2
+    dataset['w'] = 1 / dataset['vari']
+    dataset['w_sq'] = dataset['w'] ** 2
 
-    beer_hawk_inc['w_log_rr'] = beer_hawk_inc['w'] * beer_hawk_inc['log_rr']
-    beer_hawk_inc['log_rr_sq'] = beer_hawk_inc['log_rr'] ** 2
-    beer_hawk_inc['w_log_rr_sq'] = beer_hawk_inc['w'] * beer_hawk_inc['log_rr_sq']
+    dataset['w_log_rr'] = dataset['w'] * dataset['log_rr']
+    dataset['log_rr_sq'] = dataset['log_rr'] ** 2
+    dataset['w_log_rr_sq'] = dataset['w'] * dataset['log_rr_sq']
 
     # Meta-analysis of Adset
     # Group them by dimension and calculate Aggregate measure
-    for dimension, df_dim_dateset in beer_hawk_inc.groupby('dimension_type'):
+    for dimension, df_dim_dateset in dataset.groupby('dimension_type'):
         q = get_q_value(df_dim_dateset)
         tow = get_tow_value(df_dim_dateset, q)
-        beer_hawk_inc_dim = beer_hawk_inc[beer_hawk_inc['dimension_type'] == dimension]
+        beer_hawk_inc_dim = dataset[dataset['dimension_type'] == dimension]
         # adjusted weight
         beer_hawk_inc_dim['w_adj'] = 1 / (beer_hawk_inc_dim['vari'] + (tow ** 2))
         beer_hawk_inc_dim['w_adj_log_rr'] = beer_hawk_inc_dim['w_adj'] * beer_hawk_inc_dim['log_rr']
